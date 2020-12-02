5468d1118b5daa915f69b3d8ef9c30522a0edabd
hajime OGUSHI <info@m-g-n.me>
2020-12-02 22:41:37 +0900

    修正

--- megane-auto-update.php
+++ megane-auto-update.php
@@ -2,17 +2,40 @@
 /**
  * Plugin name: megane auto update plugin
  * Description: このプラグインは自動的にアップデートされることだけを目的にしたプラグインです。
- * Version: 0.0.3
+ * Version: 0.0.4
  *
  * @package megane
  * @author megane9988
  * @license GPL-2.0+
  */
 
-require_once "vendor/autoload.php";
+require_once 'vendor/autoload.php';
+use Inc2734\WP_GitHub_Plugin_Updater\Bootstrap as Updater;
 
-$updater = new Inc2734\WP_GitHub_Plugin_Updater\Bootstrap(
-	plugin_basename( __FILE__ ),
-	'megane9988',
-	'updatefromgithubplugin'
-);
+define( 'MEGANE9988_FROM_GITHUB_AUTO_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'MEGANE9988_FROM_GITHUB_AUTO_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+class Bootstrap {
+
+	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, '_plugins_loaded' ) );
+	}
+
+	public function _plugins_loaded() {
+		add_action( 'init', array( $this, '_activate_autoupdate' ) );
+	}
+
+	/**
+	 * Activate auto update using GitHub
+	 *
+	 * @return void
+	 */
+	public function _activate_autoupdate() {
+		new Updater(
+			plugin_basename( __FILE__ ),
+			'megane9988',
+			'updatefromgithubplugin',
+			[ 'homepage' => 'https://megane-blog.com', ]
+		);
+	}
+}
