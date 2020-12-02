ae7d2843c55a0d557969bd73f3ff126299420cdc
hajime OGUSHI <info@m-g-n.me>
2020-12-02 22:46:01 +0900

    0.1

--- megane-auto-update.php
+++ megane-auto-update.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: megane auto update plugin
  * Description: このプラグインは自動的にアップデートされることだけを目的にしたプラグインです。
- * Version: 0.0.4
+ * Version: 0.1.0
  *
  * @package megane
  * @author megane9988
